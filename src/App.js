852fc5b5e29acbedb7854957aa341dd12967b537
CJ <cjampion8@gmail.com>
2025-06-17 16:56:02 -0400

    CSS Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import useGetForecast from './Hooks/GetForecast.js';
+import useGetForecast from './components/GetForecast.js';
 import './style.css';
 
 function App() {
